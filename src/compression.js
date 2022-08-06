6d690a17ad0ec13ebc4e5eab373fe085a054b742
Ashwin Dara <niwhsa.d@gmail.com>
2022-08-06 15:39:24 -0700

    Refactored enc utils. Wrote compression.

--- src/compression.js
+++ src/compression.js
@@ -1,6 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
 class BitCompressionMap {
   constructor() {
-    this.mapping = [];
+    this.decodeMapping = new Map(); 
   }
 
   /** Adds the node into the BitCompressionMap. Typically, one file will have one single
@@ -8,8 +11,7 @@ class BitCompressionMap {
    * a JSON-esque format.
    */
   addBitNode(node) {
-    let rep = node.getRepresentation();
-    this.mapping.push(rep);
+    this.decodeMapping.set(node.getCharacter(), node.getBitString());
   }
 
   /** Returns the JSON representation of the BitCompressionMap.
@@ -20,7 +22,9 @@ class BitCompressionMap {
    *      B:0001 }
    */
   getJSON() {
-    // @TODO
+    let o = Object.fromEntries(this.decodeMapping);
+    let oJson = JSON.stringify(o);
+    return oJson; 
   }
 }
 
@@ -42,8 +46,99 @@ class BitNode {
   getRepresentation() {
     return [this.character, this.bitString];
   }
+
+  getCharacter() {
+    return this.character;
+  }
+
+  getBitString() {
+    return this.bitString;
+  }
+
+  getFreq() {
+    return this.frequency; 
+  }
+
 }
 
-let b = new BitNode();
-b.bitString = 100;
-console.log(b.getArea(b.bitString));
+
+/** Takes in a file path, reads all of the contents, and then returns a frequency map 
+ * containing the frequency of every single character within the specified file.
+ */
+function getFrequencyCounter(filePath) {
+  /* Reading the specified file and converting it into a string. */
+  const data = fs.readSync(filePath).toString(); 
+
+  /* Object holds the frequency of the different characters. */
+  let freq = {}; 
+  
+  for (let i=0; i < data.length; ++i) {
+    let c = data.charAt(i);
+    if (freq[c]) {
+      freq[c] ++; 
+    } else {
+      freq[c] = 1;
+    }
+  }
+
+  /* Returning a javascript object containing the frequency of every single letter 
+  found within the specified file. */
+  return freq; 
+}
+
+/** Input: freqMap - javascript object that contains the frequency of every character within a particular file. 
+ * This will typically be generated using the "getFrequencyCounter" function. 
+ * 
+ * Output: min-heap priority queue containing "BitNode" objects within the pq.  
+ */
+function getFreqPQ(freqMap) {
+  /* */
+  let pq = []; 
+
+  for (const letter in freqMap) {
+    let node = new BitNode(); 
+    node.character = letter; 
+    node.frequency = freqMap[letter];
+    pq.push(node)
+  }
+
+  pq.sort((first, second) => {return first.frequency - second.frequency; });
+  return pq; 
+}
+
+function getHuffmanTree(prioQ) {
+  let treeRoot = null; 
+
+  while (prioQ.length > 1) {
+    let first = prioQ[0]; 
+    prioQ.shift(); 
+
+    let second = prioQ[0]; 
+    prioQ.shift(); 
+
+    let parent = BitNode(); 
+    parent.character = first.getCharacter() + " - " +  second.getCharacter();  
+    parent.frequency = first.getFreq() + second.getFreq(); 
+
+    parent.left = first; 
+    parent.right = second; 
+
+    treeRoot = parent; 
+    
+    prioQ.push(parent);
+    prioQ.sort((first, second) => {return first.frequency - second.frequency; });
+  }
+  return treeRoot;
+}
+
+/** 
+ * Input: Huffman Tree. 
+ * Output: BitCompressionMap object. 
+ * 
+ * Takes in the root of a Huffman tree and converts it into a valid CompressionMap object. 
+ * The typical workflow for generating a valid compression encoding would first be to read the 
+ * file, then it would be to 
+ */
+function getCompressionMap() {
+
+}
--- src/encryption-utils.js
+++ src/encryption-utils.js
@@ -1,72 +1,125 @@
-const crypto = require('crypto')
-const path = require('path')
+const crypto = require("crypto");
+const path = require("path");
 const fs = require("fs");
 
-
-/** Generates a valid set of public and private keys for RSA. 
+/** Generates a valid set of public and private keys for RSA.
  * Will write the keys within the specified  path given by the parameter "Path". */
 function generateRsaKeys(outPath) {
-    /* Generating all of the necessary keys. */
-    const {publicKey, privateKey} = crypto.generateKeyPairSync("rsa", {modulusLength: 2048});
-    const exportedPublic = publicKey.export({type: "pkcs1",  format: "pem"});
-    const exportedPrivate = privateKey.export({type: "pkcs1",  format: "pem"});
+  /* Generating all of the necessary keys. */
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+  });
+  const exportedPublic = publicKey.export({ type: "pkcs1", format: "pem" });
+  const exportedPrivate = privateKey.export({ type: "pkcs1", format: "pem" });
 
-    /* Storing the necessary settings associated with RSA encrypting and saving the 
+  /* Storing the necessary settings associated with RSA encrypting and saving the 
     configurations within file named 'rsa-settings.json'. */
-    let rsaInformation = {
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING, 
-        oaepHash: 'sha256'
-    };
+  let rsaInformation = {
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    oaepHash: "sha256",
+  };
 
-    /* Writing public keys, private keys, and the RSA configurations to specified outPath. */
-    fs.writeFileSync(path.join(outPath.toString(), "public.pem"), exportedPublic, {encoding: 'utf-8'});
-    fs.writeFileSync(path.join(outPath.toString(), "private.pem"), exportedPrivate, {encoding: 'utf-8'});
-    fs.writeFileSync(path.join(outPath.toString(), "rsa-settings.json"), JSON.stringify(rsaInformation, null, 2)); 
-} 
-
-/** Encrypts the file specified the param "filePath". */
-function encryptFileContents(filePath, rsaPath) { 
-
-    let ext = path.extname(filePath);
-    let name = path.basename(filePath, ext);
-    const outPath = path.join(rsaPath.toString(), "encryptions", name + "-enc-$0" + ext);
-
-    const data = fs.readFileSync(filePath, { encoding: 'utf-8' });
-    const pubKey = Buffer.from( fs.readFileSync(path.join(rsaPath.toString(), "public.pem"), { encoding: 'utf-8'}));
-
-    let encData = crypto.publicEncrypt({
-        key: pubKey, 
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING, 
-        oaepHash: 'sha256'
-    }, Buffer.from(data));
-
-    fs.writeFileSync(outPath, encData.toString('base64'), { encoding: 'utf-8' });
+  /* Writing public keys, private keys, and the RSA configurations to specified outPath. */
+  fs.writeFileSync(
+    path.join(outPath.toString(), "public.pem"),
+    exportedPublic,
+    { encoding: "utf-8" }
+  );
+  fs.writeFileSync(
+    path.join(outPath.toString(), "private.pem"),
+    exportedPrivate,
+    { encoding: "utf-8" }
+  );
+  fs.writeFileSync(
+    path.join(outPath.toString(), "rsa-settings.json"),
+    JSON.stringify(rsaInformation, null, 2)
+  );
 }
 
+/** Encrypts the file specified the param "filePath". */
+function encryptFileContents(filePath, rsaPath) {
+  /* Parsing the base file name and the extension of the file to create another file later with same name.
+   * Note that the ending post-fix is important in helping labelling and creating files later on when we
+   * distribute them into chunks for faster upload and encryption. */
+  let ext = path.extname(filePath);
+  let name = path.basename(filePath, ext);
+  /* The new file created will be <original_name> + "-enc-$0". */
+  const outPath = path.join(
+    rsaPath.toString(),
+    "encryptions",
+    name + "-enc-$0" + ext
+  );
+
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  const pubKey = Buffer.from(
+    fs.readFileSync(path.join(rsaPath.toString(), "public.pem"), {
+      encoding: "utf-8",
+    })
+  );
+
+  /* Encrypting the data from the file that was read with the appropriate information
+   * needed alongside just the data we want to encrypt (e.g., padding and hash). */
+  let encData = crypto.publicEncrypt(
+    {
+      key: pubKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(data)
+  );
+
+  /* Writing encrypted data to the new file path.*/
+  fs.writeFileSync(outPath, encData.toString("base64"), { encoding: "utf-8" });
+}
+
+/** Decrypts the encrypted file given the original file name an the rsaPath.
+ * Typically, decrypted files will be stored with the original contents within a folder located
+ * under the code directory. 
+ * However, user's themselves will have the option into which directory they can store the decrypted file.
+ */
 function decryptFileContents(filePath, rsaPath, outPath = false) {
-    let ext = path.extname(filePath);
-    let name = path.basename(filePath, ext);
-    const encryptedPath = path.join(rsaPath.toString(), "encryptions", name + "-enc-$0" + ext);
+  /* Parsing the original file name and the extension of the file. */
+  let ext = path.extname(filePath);
+  let name = path.basename(filePath, ext);
 
-    const out = path.join(rsaPath.toString(), "decryptions", name + "-decrypted" + ext);
+  /* Setting up the default outPath. */
+  const encryptedPath = path.join(
+    rsaPath.toString(),
+    "encryptions",
+    name + "-enc-$0" + ext
+  );
 
-    if (!outPath || !fs.existsSync(outPath)) {
-        outPath = out; 
-    }
+  const out = path.join(
+    rsaPath.toString(),
+    "decryptions",
+    name + "-decrypted" + ext
+  );
 
+  /* Checking whether or not the user defined outPath is valid. */
+  if (!outPath || !fs.existsSync(outPath)) {
+    outPath = out;
+  }
 
-    const encData = fs.readFileSync(encryptedPath, { encoding:'utf-8' });
-    const privKey = fs.readFileSync(path.join(rsaPath.toString(), "private.pem"), {encoding : 'utf-8'});
+  /* Preparing for the function call to decrypt the encrypted data. */
+  const encData = fs.readFileSync(encryptedPath, { encoding: "utf-8" });
+  const privKey = fs.readFileSync(
+    path.join(rsaPath.toString(), "private.pem"),
+    { encoding: "utf-8" }
+  );
 
-    let decData = crypto.privateDecrypt({
-        key: privKey, 
-        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING, 
-        oaepHash: 'sha256'
-    }, Buffer.from(encData, 'base64'));
+  let decData = crypto.privateDecrypt(
+    {
+      key: privKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(encData, "base64")
+  );
 
-    fs.writeFileSync(outPath, decData.toString('utf-8'), { encoding: 'utf-8'} );
+  /* Writing the decrypted data within the specified outPath. */
+  fs.writeFileSync(outPath, decData.toString("utf-8"), { encoding: "utf-8" });
 }
 
 exports.encryptFileContents = encryptFileContents;
 exports.decryptFileContents = decryptFileContents;
-exports.generateRsaKeys = generateRsaKeys; 
+exports.generateRsaKeys = generateRsaKeys;
